be3139ecbb16e207130f8ab1eadebd764dbb27e5
Rich-Almeid4 <ricalmeida2007@gmail.com>
2025-09-14 22:53:20 -0300

    primeiros arquivos php, ja conectados com o banco e com o css pela interntet pq começou a dar erro e eu queria testar a mensagem de cadastro

---
+++ back/php/acoes.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+require 'conecta.php';
+
+if (isset($_POST['criar'])) {
+    $nome = mysqli_real_escape_string($conn, trim($_POST['nome']));
+    $senha = mysqli_real_escape_string($conn, trim($_POST['senha']) );
+    $email = mysqli_real_escape_string($conn, trim($_POST['email']) );
+    $tipo = mysqli_real_escape_string($conn, trim($_POST['tipo']) );
+
+$sql = "INSERT INTO usuario (nome, email, senha, tipo) VALUES ('$nome', '$email', '$senha', '$tipo')";
+mysqli_query($conn, $sql);
+
+if (mysqli_affected_rows($conn)>0) {
+     $_SESSION['mensagem'] = 'Usuário cadastrado!';
+    header('Location: index.php');
+    exit;
+}else {
+    $_SESSION['mensagem'] = 'ERROR!';
+    header('Location: index.php');
+    exit;
+}
+}
